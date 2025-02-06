330dde380cc6b3e71aa4a3100ba40322e5f0fb77
Juha Yoon <ijooha16@gmail.com>
2025-02-06 15:52:20 +0900

    test

---
+++ src/components/Toast.jsx
@@ -0,0 +1,21 @@
+import { ToastContainer, toast } from "react-toastify";
+
+      {/* //모든 슬롯이 찬 경우 알림창 */}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover={false}
+        theme="colored"
+        // transition={Bounce}
+      />
+
+  
+  
+      if (myPokemon.every((mon) => mon.filled)) toast(<ToastContainer />);
+      //success, error ,warning, info, question
